98e7e78f92ab66a850a209490aa3ca020c9f65b0
Giles Chamberlin <giles@pexip.com>
2014-09-05 15:46:11 +0100

    xinitrc.d added

---
+++ xinitrc.d/00-userwm.sh
@@ -0,0 +1,7 @@
+USERWM=/Users/grc/git-repos/stumpwm/stumpwm
+
+PATH=/Users/grc/bin:/opt/local/bin:$PATH
+INFOPATH=/Users/grc/info:/usr/local/share/info/:/usr/share/info/:/usr/local/share/info/
+export INFOPATH
+
+xset +fp ~/local/lib/X11/fonts
