3167c9ed27d0a19b722be761b5298f88cfee8ba3
Giles Chamberlin <giles@pexip.com>
2015-11-18 08:30:05 +0000

    Fix for unicode display in urxvt

--- xinitrc.d/00-userwm.sh
+++ xinitrc.d/00-userwm.sh
@@ -4,4 +4,5 @@ PATH=/Users/grc/bin:/opt/local/bin:$PATH
 INFOPATH=/Users/grc/info:/usr/local/share/info/:/usr/share/info/:/usr/local/share/info/
 export INFOPATH
 
-xset +fp ~/local/lib/X11/fonts
+#xset +fp ~/local/lib/X11/fonts
+export LANG=en_GB.UTF-8
